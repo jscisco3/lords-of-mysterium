06c42b656c84b34fe1b18d5422476f1b3cf862e5
Jack Scisco <jscisco@redhat.com>
2019-01-28 09:08:51 -0500

    fix up test combat events

--- src/test/kotlin/com/jscisco/lom/events/TestCombatEvents.kt
+++ src/test/kotlin/com/jscisco/lom/events/TestCombatEvents.kt
@@ -1,13 +1,20 @@
 package com.jscisco.lom.events
 
+import com.jscisco.lom.attributes.types.Combatant
+import com.jscisco.lom.attributes.types.Player
 import com.jscisco.lom.builders.EntityFactory
 import com.jscisco.lom.configuration.EventRegistration
+import com.jscisco.lom.dungeon.DungeonBuilder
+import com.jscisco.lom.dungeon.GameContext
+import com.jscisco.lom.dungeon.strategies.EmptyDungeonStrategy
 import com.jscisco.lom.extensions.GameEntity
-import org.hexworks.amethyst.api.entity.EntityType
 import org.hexworks.cobalt.logging.api.Logger
 import org.hexworks.cobalt.logging.api.LoggerFactory
+import org.hexworks.zircon.api.Screens
+import org.hexworks.zircon.api.builder.grid.TileGridBuilder
+import org.hexworks.zircon.api.data.impl.Size3D
+import org.hexworks.zircon.api.input.KeyStroke
 import org.hexworks.zircon.internal.Zircon
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -17,8 +24,26 @@ class TestCombatEvents {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
-    val attacker: GameEntity<EntityType> = EntityFactory.newPlayer()
-    val target: GameEntity<EntityType> = EntityFactory.newGoblin()
+    val attacker: GameEntity<Combatant> = EntityFactory.newPlayer()
+    val target: GameEntity<Combatant> = EntityFactory.newGoblin()
+
+    val player: GameEntity<Player> = EntityFactory.newPlayer()
+
+    val dungeonSize: Size3D = Size3D.create(100, 60, 1)
+    val visibleSize: Size3D = Size3D.create(25, 25, 1)
+
+    val dungeon = DungeonBuilder(
+            dungeonSize = dungeonSize,
+            strategy = EmptyDungeonStrategy(dungeonSize),
+            player = player
+    ).build(visibleSize, dungeonSize)
+
+    val context = GameContext(
+            dungeon = dungeon,
+            screen = Screens.createScreenFor(TileGridBuilder.newBuilder().build()),
+            input = KeyStroke('a'),
+            player = player
+    )
 
     @BeforeAll
     internal fun registerEvents() {
@@ -27,7 +52,7 @@ class TestCombatEvents {
 
     @Test
     fun testOnHitEvent() {
-        Zircon.eventBus.publish(OnHitEvent(target, attacker))
+        Zircon.eventBus.publish(OnHitEvent(context, target, attacker))
     }
 
     @Test
@@ -36,6 +61,6 @@ class TestCombatEvents {
 
     @Test
     fun testDamageEvent() {
-        Zircon.eventBus.publish(DamageEvent(target, attacker, 50))
+        Zircon.eventBus.publish(DamageEvent(context, target, attacker, 50))
     }
 }
