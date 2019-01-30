7bcae7fe20af148c6c1e63246e4f736cae59ec6f
Jack Scisco <jscisco@redhat.com>
2019-01-30 12:15:46 -0500

    some more logging work

--- src/main/kotlin/com/jscisco/lom/main.kt
+++ src/main/kotlin/com/jscisco/lom/main.kt
@@ -3,8 +3,10 @@ package com.jscisco.lom
 import com.jscisco.lom.configuration.GameConfiguration
 import com.jscisco.lom.view.StartView
 import org.hexworks.zircon.api.SwingApplications
+import java.util.*
 
 fun main(args: Array<String>) {
+    System.setProperty("session.id", UUID.randomUUID().toString())
     val application = SwingApplications.startApplication(GameConfiguration.buildAppConfig())
     application.dock(StartView())
 }
